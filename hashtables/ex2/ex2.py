53d9d350f767237b6ff3cf68384298b59fd08bdf
Anna Winther <anski.anna@gmail.com>
2020-03-13 09:05:35 +0000

    Ex2 not yet passing the test

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -20,13 +20,26 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # loop over the tickets 
+    for ticket in tickets:
         # and hash each ticket so that the startin position is the key and the destination is the value inserting the key-value pair in the hash table
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
   
-    # find the n-th location in the route by checking the hastabel for n-1
-    # set a first_ticket to be where the destination is "None"
+    # start from the first location
+    counter = 0 
+    # set a first_ticket to be where the key (source) is "None"
+    first_ticket = hash_table_retrieve(hashtable, "NONE")
+    # print('first', first_ticket)
+    route[counter] = first_ticket
+    current_ticket = first_ticket
     # then use the first ticket to set it to current ticket and loop over incrementing the counter so that we are moving to the next location each time in the loop 
+    while current_ticket is not "NONE":
+        # increment counter each time we go over 
+        counter += 1
         # Retrieve value by using current ticket as key in hash_table_retrieve
+        current_ticket = hash_table_retrieve(hashtable, current_ticket)
+        route[counter] = current_ticket
+
+    return route
     
 
 
-    pass
