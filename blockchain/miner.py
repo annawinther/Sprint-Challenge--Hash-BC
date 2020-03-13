963f4001187e075eeb8865ce486a82ba020bfc6a
Anna Winther <anski.anna@gmail.com>
2020-03-13 09:48:40 +0000

    Blockchain miner

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -26,6 +26,12 @@ def proof_of_work(last_proof):
     proof = 0
     #  TODO: Your code here
 
+    last_hash = hashlib.sha256(str(last_proof).encode()).hexdigest()
+    proof = random.SystemRandom().randint(60, 1000)
+
+    while not valid_proof(last_hash, proof):
+        proof += 1
+
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
 
@@ -40,8 +46,9 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess_hash = hashlib.sha256(str(proof).encode()).hexdigest()
 
+    return guess_hash[:6] == last_hash[-6:]
 
 if __name__ == '__main__':
     # What node are we interacting with?
