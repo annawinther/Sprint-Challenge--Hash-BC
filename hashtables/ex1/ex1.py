24b47290ddc4ab1941fecff5c11a670dcada3e3f
Anna Winther <anski.anna@gmail.com>
2020-03-13 08:33:43 +0000

    Ex1 passed tests

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -5,7 +5,7 @@ from hashtables import (HashTable,
                         hash_table_retrieve,
                         hash_table_resize)
 
-weights = [1, 4, 5, 6, 7]
+# weights = [1, 4, 5, 6, 7]
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
@@ -19,15 +19,24 @@ def get_indices_of_item_weights(weights, length, limit):
         answer = None
     
     # hash_table_insert(ht, weights[0], 0)
-    answer = ['test', 'working']
+    # answer = ['test', 'working']
         
-    print_answer(answer)
+    # print_answer(answer)
 
     # loop through one and insert the weight at index and the index to the hashtable
+    for i in range(0, length):
+        hash_table_insert(ht, weights[i], i)
 
-    # loop through again this time retrieving the weight and index using the difference between the limit and and the wieght at index
+    # loop through again this time retrieving the weight and index using the difference between the limit and and the wieght at index 
+    for i in range(0, length):
+        difference = limit - weights[i]
+    # retrieve from the hastable and set it to a variable 
+        retriveIndex = hash_table_retrieve(ht, difference)
+    # as long as there is something there at that index, return the variable and the index 
+        if retriveIndex is not None:
+            print('yo', retriveIndex, i)
+            return (retriveIndex, i)
     
-    # retrieve from the hastable.
     return None
 
 
@@ -37,4 +46,4 @@ def print_answer(answer):
     else:
         print("None")
 
-get_indices_of_item_weights(weights, 2, 1)
+# get_indices_of_item_weights(weights, 2, 1)
